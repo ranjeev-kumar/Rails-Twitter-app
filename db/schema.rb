c4b19b4b31664b3d99a6cb48819089e0aca1022f
ranjeev-kumar <ranjeev.kumar@wwindia.com>
2016-03-11 13:33:44 +0530

    Added postgres database with application

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160307142007) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "tweet_id"
@@ -21,8 +24,8 @@ ActiveRecord::Schema.define(version: 20160307142007) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "comments", ["tweet_id"], name: "index_comments_on_tweet_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+  add_index "comments", ["tweet_id"], name: "index_comments_on_tweet_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
@@ -31,9 +34,9 @@ ActiveRecord::Schema.define(version: 20160307142007) do
     t.datetime "updated_at",  null: false
   end
 
-  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
-  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
 
   create_table "tweets", force: :cascade do |t|
     t.text     "body"
@@ -42,7 +45,7 @@ ActiveRecord::Schema.define(version: 20160307142007) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "tweets", ["user_id"], name: "index_tweets_on_user_id"
+  add_index "tweets", ["user_id"], name: "index_tweets_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "fname"
@@ -67,7 +70,10 @@ ActiveRecord::Schema.define(version: 20160307142007) do
     t.datetime "avatar_updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "tweets"
+  add_foreign_key "comments", "users"
+  add_foreign_key "tweets", "users"
 end
